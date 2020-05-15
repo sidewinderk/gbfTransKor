abaaadffc03e3dfe3c123819a3369ea83a2dc865
SideWinder <sidewinderkr@gmail.com>
2020-05-15 09:13:35 +0200

    revert observers

--- content.js
+++ content.js
@@ -667,7 +667,7 @@ const main = async () => {
   InitList();
   PrintLog("ORIGIN: "+generalConfig.origin);
   try {
-    await Promise.all(ObserverImage()); //ObserverImageDIV(),ObserverImage(), ObserveNameText(), ObserveSceneText(),ObserverArchive(), ObserverPop(), ObserverBattle(),ObserverArchive()
+    await Promise.all(ObserverImageDIV(),ObserverImage(), ObserveNameText(), ObserveSceneText(),ObserverArchive(), ObserverPop(), ObserverBattle(),ObserverArchive(),ObserverImage()); //
   } catch (e) {
     PrintLog(e);
   }
