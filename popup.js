fd45ab9dda232601150590e5be4521af9f898caa
SideWinder <sidewinderkr@gmail.com>
2020-06-07 00:10:08 +0200

    Revert "텍스트 추출&번역 기능 완벽 적용"

--- popup.js
+++ popup.js
@@ -1,94 +1,84 @@
 document.addEventListener('DOMContentLoaded', function() {
-	(function() {
-		chrome.tabs.query(
-			{
-				active: true,
-				lastFocusedWindow: true
-			},
-			function(tabs) {
-				chrome.tabs.sendMessage(tabs[0].id, { data: 'update', text: '' });
-			}
-		);
-	})();
+    (function() {
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { data: "update", text: "" });
+        });
+    })()
 
-	let getNameBtn = document.getElementById('getName');
-	let copyNameBtn = document.getElementById('copyName');
-	let clearNameBtn = document.getElementById('cacheClearName');
-	let downNameBtn = document.getElementById('downName');
-	let nameout = document.getElementById('namecheck');
+    // Restore options
+    chrome.storage.local.get(['extractMode', 'translateMode'], function(items) {
+        document.getElementById('translateModeChecker').checked = items.translateMode;
+        document.getElementById('extractModeChecker').checked = items.extractMode;
+        if (items.extractMode)
+            document.getElementById('extractModeWindow').style.display = 'block';
+    });
 
-	let getMiscBtn = document.getElementById('getMisc');
-	let copyMiscBtn = document.getElementById('copyMisc');
-	let clearMiscBtn = document.getElementById('cacheClearMisc');
-	let downMiscBtn = document.getElementById('downMisc');
-	let othersout = document.getElementById('otherscheck');
+    let getTextBtn = document.getElementById('getText');
+    let copyTextBtn = document.getElementById('copyText');
+    let clearTextBtn = document.getElementById('cacheClearText');
+    let downTextBtn = document.getElementById('downText');
+    let textout = document.getElementById("outputcheck");
 
-	let updateBtn = document.getElementById('update');
+    let getNameBtn = document.getElementById('getName');
+    let copyNameBtn = document.getElementById('copyName');
+    let clearNameBtn = document.getElementById('cacheClearName');
+    let downNameBtn = document.getElementById('downName');
+    let nameout = document.getElementById("namecheck");
 
-	let getScenesBtn = document.getElementById('getScenes');
-	let ScenesOut = document.getElementById('Scenes');
-	let clearScenes = document.getElementById('clearScenes');
-	let downScenes = document.getElementById('downScenes');
-	let copyScenes = document.getElementById('copyScenes');
+    let getMiscBtn = document.getElementById('getMisc');
+    let copyMiscBtn = document.getElementById('copyMisc');
+    let clearMiscBtn = document.getElementById('cacheClearMisc');
+    let downMiscBtn = document.getElementById('downMisc');
+    let othersout = document.getElementById("otherscheck");
 
-	copyScenes.onclick = function(element) {
-		ScenesOut.select();
-		ScenesOut.setSelectionRange(0, 1e8);
-		document.execCommand('copy');
-	};
-	downScenes.onclick = function(element) {
-		var result = confirm('Download story text csv file?');
-		if (result) {
-			var a = document.createElement('a');
-			with (a) {
-				href = 'data:text/csv;charset=urf-8,' + encodeURIComponent(ScenesOut.value);
-				download = 'quest.csv';
-			}
-			document.body.appendChild(a);
-			a.click();
-			document.body.removeChild(a);
-		}
-	};
+    let updateBtn = document.getElementById('update');
 
-	getScenesBtn.onclick = function(element) {
-		chrome.storage.local.get(['sceneFullInfo'], function(result) {
-			if (typeof result.sceneFullInfo == 'undefined') return;
-			//console.log(result.sceneFullInfo);
-			var text = 'Language,SceneCode,Type,Name,Origin,Korean\n';
 
-			result.sceneFullInfo.forEach(function(scene) {
-				console.log(scene);
-				text = text + scene.Language + ',';
-				text = text + scene.SceneCode + ',';
-				text = text + scene.Type + ',';
-				text = text + scene.Name + ',';
-				text = text + scene.Origin + ',\n';
-			});
+    updateBtn.onclick = function(element) {
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { data: "refresh", text: "" });
+        });
+        textout.value = "";
+        nameout.value = "";
+        othersout.value = "";
+    }
 
-			ScenesOut.value = text;
-		});
-	};
 
-	clearScenes.onclick = function(element) {
-		chrome.tabs.query(
-			{
-				active: true,
-				lastFocusedWindow: true
-			},
-			function(tabs) {
-				chrome.tabs.sendMessage(tabs[0].id, { data: 'clearScenes', text: '' });
-			}
-		);
-		ScenesOut.value = '';
-	};
+    getTextBtn.onclick = function(element) {
+        chrome.storage.local.get(['oTEXT'], function(result) {
+            var outputtext = "index,sceneCode,orig,kr\n"
+            result.oTEXT.forEach(function(json) {
+                outputtext = outputtext + json.index + ",[" + json.sceneCode + "]," + json.orig + ",\n"
+            });
+            textout.value = outputtext;
+        });
+    }
+    copyTextBtn.onclick = function(element) {
+        textout.select();
+        textout.setSelectionRange(0, 1e8);
+        document.execCommand("copy");
+    }
+    downTextBtn.onclick = function(element) {
+        var result = confirm("Download story text csv file?");
+        if (result) {
+            var a = document.createElement('a');
+            with(a) {
+                href = 'data:text/csv;charset=urf-8,' + encodeURIComponent(textout.value);
+                download = 'storyText.csv';
+            }
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        }
+    }
+    clearTextBtn.onclick = function(element) {
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { data: "clearText", text: "" });
+        });
+        textout.value = "";
+    }
 
-	updateBtn.onclick = function(element) {
-		chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, { data: 'refresh', text: '' });
-		});
-	};
-
-	getNameBtn.onclick = function(element) {
+    getNameBtn.onclick = function(element) {
         chrome.storage.local.get(['nTEXT'], function(result) {
             var outputtext = "orig,kr\n"
             result.nTEXT.forEach(function(element) {
@@ -155,41 +145,33 @@ document.addEventListener('DOMContentLoaded', function() {
         });
         othersout.value = "";
     }
-	
-	document.getElementById('go-to-options').onclick = function(element) {
-		if (chrome.runtime.openOptionsPage) {
-			chrome.runtime.openOptionsPage();
-		} else {
-			window.open(chrome.runtime.getURL('options.html'));
-		}
-	};
 
-	// Translate mode
-	document.getElementById('translateModeChecker').onclick = function() {
-		var transModeCurrent = document.getElementById('translateModeChecker').checked;
-		chrome.storage.local.set(
-			{
-				translateMode: transModeCurrent
-			},
-			function() {}
-		);
-	};
+    document.getElementById('go-to-options').onclick = function(element) {
+        if (chrome.runtime.openOptionsPage) {
+            chrome.runtime.openOptionsPage();
+        } else {
+            window.open(chrome.runtime.getURL('options.html'));
+        }
+    }
 
-	// DIV
-	document.getElementById('extractModeChecker').onclick = function() {
-		var extractModeCurrent = document.getElementById('extractModeChecker').checked;
-		chrome.storage.local.set(
-			{
-				extractMode: extractModeCurrent
-			},
-			function() {}
-		);
-		if (document.getElementById('extractModeChecker').checked) {
-			document.getElementById('extractModeWindow').style.display = 'block';
-		} else {
-			document.getElementById('extractModeWindow').style.display = 'none';
-		}
-	};
-	
+    // Translate mode
+    document.getElementById('translateModeChecker').onclick = function() {
+        var transModeCurrent = document.getElementById('translateModeChecker').checked;
+        chrome.storage.local.set({
+            translateMode: transModeCurrent
+        }, function() {});
+    }
 
+    // DIV
+    document.getElementById('extractModeChecker').onclick = function() {
+        var extractModeCurrent = document.getElementById('extractModeChecker').checked;
+        chrome.storage.local.set({
+            extractMode: extractModeCurrent
+        }, function() {});
+        if (document.getElementById('extractModeChecker').checked) {
+            document.getElementById('extractModeWindow').style.display = 'block';
+        } else {
+            document.getElementById('extractModeWindow').style.display = 'none';
+        }
+    }
 });
