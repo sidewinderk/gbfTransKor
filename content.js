8eb5e21b99b896318715e884ef1dbe8d52e1a3ad
ajh1000 <moulawola@gmail.com>
2020-11-12 11:24:43 +0900

    Update content.js
    Co-authored-by: SideWinder <sidewinderkr@gmail.com>

--- content.js
+++ content.js
@@ -2726,16 +2726,10 @@ async function ObserverImageDIV() {
         window.setTimeout(ObserverImage, generalConfig.refreshRate);
         return;
     }
-    if (doBattleTrans ) {
-        if(doc.URL.includes('#raid')){
-            ImageObserver.disconnect();
-            window.setTimeout(ObserverImageDIV, generalConfig.refreshRate);
-            return;
-        }
-    }
     if(doc.URL.includes('#raid')){
+        ImageObserver.disconnect();
         archiveObserver.disconnect();
-        window.setTimeout(ObserverArchive, generalConfig.refreshRate);
+        window.setTimeout(ObserverImageDIV, generalConfig.refreshRate);
         return;
     }
     ImageObserverDIV.observe(allElements, config_image);
