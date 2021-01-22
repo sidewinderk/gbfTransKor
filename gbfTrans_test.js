bacae0141262ba5304421c2319bcfe39302e712d
ajh1000 <moulawola@gmail.com>
2021-01-23 01:32:04 +0900

    전투 텍스트 숫자 번역 오류 해결

--- gbfTrans_test.js
+++ gbfTrans_test.js
@@ -1962,7 +1962,7 @@ async function InitList() {
         }
 
         //캔버스 텍스트 번역
-        if (transMode) {
+        if (transMode || exMode) {
             var origCanvasRenderingContext = CanvasRenderingContext2D.prototype.fillText;
             CanvasRenderingContext2D.prototype.fillText = function () {
                 if (arguments.length <= 1) {
@@ -1972,6 +1972,12 @@ async function InitList() {
 
                 var stext = arguments[0];
                 window_PrintLog(arguments);
+
+                // If the text contains any number, save the number and replace it to "*"
+                var number = stext.replace(/[^0-9]/g, '');
+                if (number.length > 0) {
+                    stext = stext.replace(/[0-9]/g, '*');
+                }
 
                 if (transMode) {
                     var transText = '';
@@ -1988,6 +1994,13 @@ async function InitList() {
                     });
                     if (transText) {
                         if (transText.length > 0) {
+                            if (number.length > 0) {
+                                // If it contains number("*"), recover it from the saved number
+                                for (var i = 0; i < number.length; i++) {
+                                    transText = transText.slice(0, transText.indexOf('*')) + number[i] + transText.slice(transText.indexOf('*') + 1);
+                                }
+                            }
+
                             arguments[0] = transText;
                         }
                     }
