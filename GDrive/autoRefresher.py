321005b9a21c1068c34db33a213aece430a9abce
SideWinder <sidewinderkr@gmail.com>
2021-09-07 22:31:47 +0900

    fix typo

--- GDrive/autoRefresher.py
+++ GDrive/autoRefresher.py
@@ -1,7 +1,7 @@
 import gsheets
 import mergesheets
 
-listOfFiles = ["archive", "name", "quest", "image","battle", "conjuction"]
+listOfFiles = ["archive", "name", "quest", "image","battle", "conjunction"]
 
 if(__name__ == '__main__'):
     for sht in gsheets.SHEETS:
